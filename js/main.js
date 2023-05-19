184bfb49ae10abfaa327fc4e00dfcbbb443b2a87
n0g4y0 <n0g4y0@gmail.com>
2023-05-18 23:59:23 -0400

    agrega clase principal del juego llamado Tarjeta

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
+class Tarjeta{
+    constructor(color){
+        this._color = color;
+    }
+    get color(){
+        return this._color;
+    }
+    
+    get value(){
+        return  this.color[0].toUpperCase();
+    }
+}
 
 
